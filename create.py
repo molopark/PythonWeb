904a03f928fb0d94b5ee58c36ce3ef50845fd8f8
molopark <molopark@gmail.com>
2018-12-10 13:40:28 +0900

    파일 생성/수정/삭제

--- create.py
+++ create.py
@@ -14,18 +14,9 @@ form = cgi.FieldStorage()
 if 'id' in form:
     pageId = form["id"].value
     description = open('data/'+pageId, 'r').read()
-    update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
-    delete_action = '''
-        <form action="process_delete.py" method="post">
-            <input type="hidden" name="pageId" value="{}">
-            <input type="submit" value="delete">
-        </form>
-    '''.format(pageId)
 else:
     pageId = 'Welcome'
     description = 'Hello, web'
-    update_link = ''
-    delete_action = ''
 
 print(
 '''
@@ -39,11 +30,12 @@ print(
   <h1><a href="index.py">WEB</a></h1>
   <ol>{listStr}</ol>
   <a href="create.py">create</a>
-  {update_link}
-  {delete_action}
-  <h2>{title}</h2>
-  <p>{desc}</p>
+  <form action="process_create.py" method="post">
+    <p><input type="text" name="title" placeholder="title"></p>
+    <p><textarea name="description" rows="4" placeholder="description"></textarea></p>
+    <p><input type="submit"></p>
+  </form>
 </body>
 </html>
-'''.format(title=pageId, desc=description, listStr=listStr, update_link=update_link, delete_action=delete_action)
+'''.format(title=pageId, desc=description, listStr=listStr)
 )
--- update.py
+++ update.py
@@ -14,18 +14,9 @@ form = cgi.FieldStorage()
 if 'id' in form:
     pageId = form["id"].value
     description = open('data/'+pageId, 'r').read()
-    update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
-    delete_action = '''
-        <form action="process_delete.py" method="post">
-            <input type="hidden" name="pageId" value="{}">
-            <input type="submit" value="delete">
-        </form>
-    '''.format(pageId)
 else:
     pageId = 'Welcome'
     description = 'Hello, web'
-    update_link = ''
-    delete_action = ''
 
 print(
 '''
@@ -39,11 +30,13 @@ print(
   <h1><a href="index.py">WEB</a></h1>
   <ol>{listStr}</ol>
   <a href="create.py">create</a>
-  {update_link}
-  {delete_action}
-  <h2>{title}</h2>
-  <p>{desc}</p>
+  <form action="process_update.py" method="post">
+    <input type="hidden" name="pageId" value="{form_default_title}"
+    <p><input type="text" name="title" placeholder="title" value="{form_default_title}"></p>
+    <p><textarea name="description" rows="4" placeholder="description">{form_default_description}</textarea></p>
+    <p><input type="submit"></p>
+  </form>
 </body>
 </html>
-'''.format(title=pageId, desc=description, listStr=listStr, update_link=update_link, delete_action=delete_action)
+'''.format(title=pageId, desc=description, listStr=listStr, form_default_title=pageId, form_default_description=description)
 )
